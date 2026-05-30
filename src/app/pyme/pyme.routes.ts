00b6b9d44b150003c7cea265b55f452011f91974
Brayan Luna <brayanlmonrroy@gmail.com>
2026-05-30 12:45:31 -0400

    default path redirect to login page
    - remove ngmodules
    - use standalone components
    - include date limit in marketplace

--- src/app/pyme/pyme.routes.ts
+++ src/app/pyme/pyme.routes.ts
@@ -1,11 +1,13 @@
 import type { Routes } from '@angular/router';
+import { LoginComponent } from '../auth/login.component';
 import { ImpactDashboardComponent } from './components/impact-dashboard.component';
 import { LotDetailComponent } from './components/lot-detail.component';
 import { MarketplaceComponent } from './components/marketplace.component';
 
 export const pymeRoutes: Routes = [
+    // {path: '/', component: }
     { path: 'marketplace', component: MarketplaceComponent },
     { path: 'marketplace/:id', component: LotDetailComponent },
     { path: 'impact', component: ImpactDashboardComponent },
-    { path: '', redirectTo: 'marketplace', pathMatch: 'full' },
+    { path: '', component: LoginComponent },
 ];
